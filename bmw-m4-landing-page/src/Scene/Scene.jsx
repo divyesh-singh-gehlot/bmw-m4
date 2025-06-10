861ed72d0615a5f8a738a435442373c0b4be760d
DIVYESH SINGH GEHLOT <gehlotdivyeshsingh@gmail.com>
2025-06-10 17:00:34 +0530

    Added Camera Rotation

--- bmw-m4-landing-page/src/Scene/Scene.jsx
+++ bmw-m4-landing-page/src/Scene/Scene.jsx
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 import { Environment, PerspectiveCamera, OrbitControls } from '@react-three/drei'
 import { Bmw } from '../assets/bmw'
 import { useFrame } from '@react-three/fiber';
+import gsap from 'gsap';
+
 
 
 const Scene = ({ progress }) => {
@@ -13,21 +15,55 @@ const Scene = ({ progress }) => {
 
   useEffect(() => {
     const updateCamPos = () => {
-      const poitions = [[8.93, 1.27, 0.22],
-      [4.96,2.14,7.80],
-      [-0.09,1.83,9.30],
-      [1.58,9.48,-0.00],
+      const positions = [[8.93, 1.27, 0.22],
+      [4.96, 2.14, 7.80],
+      [-0.09, 1.83, 9.30],
+      [-0.09, 1.83, 9.30],
+      [-0.09, 1.83, 9.30],
+      [1.58, 9.48, -0.00],
       [-4.54, 4.66, 5.56],
       [3.07, 2.76, 4.20],
       [-2.06, 1.98, -4.90],
       ]
+
+      if (progress >= 1) {
+        gsap.to(cameraRef.current.position, {
+          x: -2.06,
+          y: 1.98,
+          z: -4.90,
+          duration: .5,
+          ease: 'power1.out',
+        })
+      }
+      else {
+        const segmentProgress = 1 / 8;
+        const segmentIndex = Math.floor(progress / segmentProgress)
+        // console.log(segmentIndex)
+        const percentage = (progress % segmentProgress) / segmentProgress
+        const [startX, startY, startZ] = positions[segmentIndex];
+        const [endX, endY, endZ] = positions[segmentIndex + 1];
+        const x = startX + (endX - startX) * percentage;
+        const y = startY + (endY - startY) * percentage;
+        const z = startZ + (endZ - startZ) * percentage;
+        gsap.to(cameraRef.current.position, {
+          x,
+          y,
+          z,
+          duration: .5,
+          ease: 'power1.out',
+        }
+        )
+      }
     }
-  }, [progress])
+
+
+    updateCamPos()
+  }, [progress, cameraRef.current])
 
 
   return (
     <>
-      <OrbitControls />
+      {/* <OrbitControls /> */}
       <PerspectiveCamera ref={cameraRef}
         fov={45} near={0.1}
         far={10000} makeDefault
